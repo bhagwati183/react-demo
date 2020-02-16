e423542b4f87adccc105f4ea6fbb2a875be1eae7
bhagwati183 <bhagwatinaik18.bn@gmail.com>
2020-02-16 14:32:44 +0530

    First React Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
-function App() {
+const App=()=> {
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <table>
+    <thead>
+  <tr>
+    <th colSpan="2">My Bio</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <th>Name</th>
+      <td>{data.Name}</td>
+      </tr>
+    
+    <tr>
+      <th>Age</th>
+      <td>{data.Age}</td>
+    </tr>
+    <tr>
+      <th>Address</th>
+      <td>{data.Address}</td>
+    </tr>
+    <tr>
+      <th>Hobbies</th>
+      <td>{data.Hobbies}</td>
+    </tr>
+    </tbody>
+    </table>
+    
+    <button onClick={()=>ClickMe(data.Name)}>Greet</button>
+    </>
+   
   );
 }
-
+const ClickMe=name=>{
+  return (
+    alert(`hi i am,${name}`)
+  )
+}
 export default App;
+
